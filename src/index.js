4b596ad0c0971e066f39f83d4e57d08739e0d87b
UlianaFomina <6758599@gmail.com>
2021-03-08 01:19:05 +0300

    feat: add avg

--- src/index.js
+++ src/index.js
@@ -18,5 +18,9 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum=0;
+  for(let i=0; i<array.length; i++){
+  sum+=array[i];
+  }
+  return sum/array.length;
 }
