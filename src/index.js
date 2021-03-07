fe7a8cc8307003be780a7584f668a58269b94e8a
UlianaFomina <6758599@gmail.com>
2021-03-08 01:18:15 +0300

    feat: add max

--- src/index.js
+++ src/index.js
@@ -9,7 +9,12 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maximum=0;
+  for(let i=0; i< array.length; i++){
+    if (array[i]>maximum)
+     maximum=array[i];
+  }
+  return maximum;
 }
 
 exports.avg = function avg (array) {
